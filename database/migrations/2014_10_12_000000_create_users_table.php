fc569b12f51ba3b47c8de0cfb43c066675684f8a
João Manoel <joaomanoelskai@gmail.com>
2026-09-19 18:37:22 -0300

    change: alterando a tabela de users pela primeira vez

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('ip_address', 45)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
